c9cbe07e62aaea78ee3db94ecdd7b54a515ab37f
Goudet Ethan <ethangoudet@gmail.com>
2025-01-23 10:20:21 +0100

    fetch perso normal

--- src/Components/Fetchcards/FetchCharacters.jsx
+++ src/Components/Fetchcards/FetchCharacters.jsx
@@ -1,27 +1,7 @@
-import { useState, useEffect } from 'react';
 import Comment from "../Comments/Comments";
 import Apifetch from "../../api/Apifetch";
 
 export default function FetchCharacters() {
-  const [selectedCharacterId, setSelectedCharacterId] = useState(null);
-  const [games, setGames] = useState([]);
-
-  useEffect(() => {
-    if (selectedCharacterId) {
-      fetch(`https://zelda.fanapis.com/api/games/${selectedCharacterId}`)
-        .then(response => response.json())
-        .then(data => {
-          console.log('Games data:', data);
-          setGames(data.games || []);
-        })
-        .catch(error => console.error('Error fetching games:', error));
-    }
-  }, [selectedCharacterId]);
-
-  const extractIdFromUrl = (url) => {
-    const parts = url.split('/');
-    return parts[parts.length - 1];
-  };
 
   return (
     <>
@@ -37,7 +17,6 @@ export default function FetchCharacters() {
                   .map((charact) => (
                     <div key={charact.id} style={{border: "1px solid black", margin: "10px", padding: "30px", backgroundColor: "black", color: "white"}}>
                         <p>{charact.name}</p>
-                        <button onClick={() => setSelectedCharacterId(extractIdFromUrl(charact.appearances))}>Show Games</button>
                         <div>
                           <Comment />
                         </div>
@@ -50,20 +29,6 @@ export default function FetchCharacters() {
           );
         }}
       </Apifetch>
-      {selectedCharacterId && (
-        <div>
-          <h2>Games for Character ID: {selectedCharacterId}</h2>
-          <ul>
-            {games.length > 0 ? (
-              games.map(game => (
-                <li key={game.id}>{game.name}</li>
-              ))
-            ) : (
-              <p>No games found for this character.</p>
-            )}
-          </ul>
-        </div>
-      )}
     </>
   );
 }
